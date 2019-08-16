553246b2798e3929f9220766285153a04eb8275a
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2019-08-16 13:01:22 -0300

    media: lib/sort.c: implement sort() variant taking context argument
    Our list_sort() utility has always supported a context argument that
    is passed through to the comparison routine. Now there's a use case
    for the similar thing for sort().
    
    This implements sort_r by simply extending the existing sort function
    in the obvious way. To avoid code duplication, we want to implement
    sort() in terms of sort_r(). The naive way to do that is
    
    static int cmp_wrapper(const void *a, const void *b, const void *ctx)
    {
      int (*real_cmp)(const void*, const void*) = ctx;
      return real_cmp(a, b);
    }
    
    sort(..., cmp) { sort_r(..., cmp_wrapper, cmp) }
    
    but this would do two indirect calls for each comparison. Instead, do
    as is done for the default swap functions - that only adds a cost of a
    single easily predicted branch to each comparison call.
    
    Aside from introducing support for the context argument, this also
    serves as preparation for patches that will eliminate the indirect
    comparison calls in common cases.
    
    Requested-by: Boris Brezillon <boris.brezillon@collabora.com>
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Acked-by: Andrew Morton <akpm@linux-foundation.org>
    Tested-by: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- lib/sort.c
+++ lib/sort.c
@@ -142,9 +142,12 @@ static void do_swap(void *a, void *b, size_t size, swap_func_t swap_func)
 		swap_func(a, b, (int)size);
 }
 
+typedef int (*cmp_func_t)(const void *, const void *);
+typedef int (*cmp_r_func_t)(const void *, const void *, const void *);
 #define _CMP_WRAPPER ((cmp_r_func_t)0L)
 
-static int do_cmp(const void *a, const void *b, cmp_r_func_t cmp, const void *priv)
+static int do_cmp(const void *a, const void *b,
+		  cmp_r_func_t cmp, const void *priv)
 {
 	if (cmp == _CMP_WRAPPER)
 		return ((cmp_func_t)(priv))(a, b);
@@ -197,8 +200,8 @@ static size_t parent(size_t i, unsigned int lsbit, size_t size)
  * it less suitable for kernel use.
  */
 void sort_r(void *base, size_t num, size_t size,
-	    cmp_r_func_t cmp_func,
-	    swap_func_t swap_func,
+	    int (*cmp_func)(const void *, const void *, const void *),
+	    void (*swap_func)(void *, void *, int size),
 	    const void *priv)
 {
 	/* pre-scale counters for performance */
@@ -264,8 +267,8 @@ void sort_r(void *base, size_t num, size_t size,
 EXPORT_SYMBOL(sort_r);
 
 void sort(void *base, size_t num, size_t size,
-	  cmp_func_t cmp_func,
-	  swap_func_t swap_func)
+	  int (*cmp_func)(const void *, const void *),
+	  void (*swap_func)(void *, void *, int size))
 {
 	return sort_r(base, num, size, _CMP_WRAPPER, swap_func, cmp_func);
 }
