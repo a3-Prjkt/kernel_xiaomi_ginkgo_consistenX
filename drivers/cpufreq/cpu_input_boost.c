e76a7f87c41af543cb0461bc4670c71f28e7da0b
Adithya R <radithya2002@gmail.com>
2020-05-12 11:38:56 +0530

    cpu_input_boost: Don't boost on input event
     * For some power saving
    
    Signed-off-by: fajar <santuyz321@gmail.com>
    Signed-off-by: Disconnnected0 <womhateui@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -274,6 +274,8 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 					unsigned int type, unsigned int code,
 					int value)
 {
+	return;
+
 	struct boost_drv *b = handle->handler->private;
 
 	b->last_input_jiffies = jiffies;
