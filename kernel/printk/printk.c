81e55744337b30918b49ab21e080b8ab052a0611
wloot <wloot@qq.com>
2021-01-04 17:30:15 +0200

    printk: silence healthd and batteryd logs
    * <3>batteryd, <4>healthd
    
    * sum fixup
    
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>
    Signed-off-by: Disconnnected0 <womhateui@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -801,6 +801,10 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 	 */
 	line = buf;
 	if (line[0] == '<') {
+		if (memcmp(line+3, "batteryd", sizeof("batteryd")-1) == 0 ||
+			   memcmp(line+3, "healthd", sizeof("healthd")-1) == 0)
+			goto ignore;
+		{
 		char *endp = NULL;
 		unsigned int u;
 
@@ -813,9 +817,13 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			len -= endp - line;
 			line = endp;
 		}
+		}
 	}
 
 	printk_emit(facility, level, NULL, 0, "%s", line);
+	return ret;
+ignore:
+	kfree(buf);
 	return ret;
 }
 
