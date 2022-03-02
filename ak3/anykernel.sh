176a7c95a5e7eadbde23188958969cb7e75eec69
Forest <74813226+Disconnect0@users.noreply.github.com>
2022-03-02 08:53:50 +0800

    anykernel.sh: Changed some stuffs

--- ak3/anykernel.sh
+++ ak3/anykernel.sh
@@ -4,18 +4,18 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=STRIX ginkgo Kernel by fiqri19102002 @ github
+kernel.string=Cons!stenX Kernel by Disconnect0 (ForestD)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=ginkgo
-device.name2=
+device.name2=willow
 device.name3=
 device.name4=
 device.name5=
-supported.versions=11-12
+supported.versions=10.0-12.0
 supported.patchlevels=
 '; } # end properties
 
@@ -24,9 +24,11 @@ block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
+
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
+
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
@@ -36,19 +38,6 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 ## AnyKernel boot install
 dump_boot;
-
-# begin ramdisk changes
-
-# Set Android version for kernel
-ver="$(file_getprop /system/build.prop ro.build.version.release)"
-if [ ! -z "$ver" ]; then
-  patch_cmdline "androidboot.version" "androidboot.version=$ver"
-else
-  patch_cmdline "androidboot.version" ""
-fi
-
-# end ramdisk changes
-
 write_boot;
 ## end boot install
 
@@ -67,4 +56,3 @@ write_boot;
 
 #flash_boot;
 ## end vendor_boot install
-
