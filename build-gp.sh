78bdca761fe9475d909816e794c03598e13e4529
Forest <forestdanielolbis@gmail.com>
2022-03-11 00:14:31 +0000

    Renamed and Updated the description of 'build-gp.sh'

--- build-gp.sh
+++ build-gp.sh
@@ -2,6 +2,9 @@
 #
 # Compile script for ConsIstenX/QuicksilveR kernel
 # Copyright (C) 2020-2021 Adithya R.
+# Made this for Gitpod Users
+# Because of Reinstallation the TC again after the Workspace is Closed
+# This Modifed TC_DIR Would save you some Precious Time - Disconnect0
 
 SECONDS=0 # builtin bash timer
 ZIPNAME="CIsX~v1-ginkgo-$(date '+%Y%m%d-%H%M').zip"
