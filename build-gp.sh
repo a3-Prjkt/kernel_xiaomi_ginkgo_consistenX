de55e6830d687d34229f5cb9e9b43cf592491d4d
Forest <forestd.github@gmail.com>
2022-03-14 12:36:09 +0000

    Revert "Merge pull request #9 from a3-Prjkt/eleven"
    This reverts commit c1ca22113469e98bebf22712b90eaa3e74f5339d, reversing
    changes made to 32d3148cdf03250e2eea5649e2952cbaf18a853a.

---
+++ build-gp.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# Compile script for ConsIstenX/QuicksilveR kernel
+# Copyright (C) 2020-2021 Adithya R.
+# Made this for Gitpod Users
+# Because of Reinstallation the TC again after the Workspace is Closed
+# This Modifed TC_DIR Would save you some Precious Time - Disconnect0
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="CIsX~v1-ginkgo-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="/workspace/tc"
+AK3_DIR="./ak3"
+DEFCONFIG="vendor/ginkgo-perf_defconfig"
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+echo "xRageTC clang not found! Cloning to $TC_DIR..."
+if ! git clone -q -b main --depth=1 https://github.com/xyz-prjkt/xRageTC-clang $TC_DIR; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+export KBUILD_BUILD_USER=forest
+export KBUILD_BUILD_HOST=Disconnect0
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+cp out/defconfig arch/arm64/configs/$DEFCONFIG
+exit
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+rm -rf out
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
+
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
+if [ -d "$AK3_DIR" ]; then
+cp -r $AK3_DIR AnyKernel3
+elif ! git clone -q https://github.com/Disconnect0/AnyKernel3; then
+echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+exit 1
+fi
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+cp out/arch/arm64/boot/dtbo.img AnyKernel3
+rm -f *zip
+cd AnyKernel3
+git checkout master &> /dev/null
+zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+cd ..
+rm -rf AnyKernel3
+rm -rf out/arch/arm64/boot
+echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+echo "Zip: $ZIPNAME"
+if ! [[ $HOSTNAME = "RyzenBeast" && $USER = "adithya" ]]; then
+curl -T $ZIPNAME temp.sh; echo
+fi
+else
+echo -e "\nCompilation failed!"
+fi
