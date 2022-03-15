471a7713c88df03c06c66b01fb9541476bc4447a
Forest <forestd.github@gmail.com>
2022-03-15 00:41:31 +0000

    build-gp: edited directory

--- build-gp.sh
+++ build-gp.sh
@@ -8,7 +8,7 @@
 
 SECONDS=0 # builtin bash timer
 ZIPNAME="CIsX~v1-ginkgo-$(date '+%Y%m%d-%H%M').zip"
-TC_DIR="workspace/tc"
+TC_DIR="/workspace/tc"
 AK3_DIR="./ak3"
 DEFCONFIG="vendor/ginkgo-perf_defconfig"
 
