ddc254001d0fcc440baead24aa620c1fd5209118
Forest <forestd.github@gmail.com>
2022-04-23 14:15:55 +0000

    Revert "cpuidle: lpm_levels: add soft watchdog for s2idle"
    This reverts commit 67d888ff1a36d6bc4f1ccc3604eded81a8a6c875.
    
    Signed-off-by: Forest <forestd.github@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -85,8 +85,6 @@ struct lpm_debug {
 
 #define MAX_S2IDLE_CPU_ATTEMPTS  32   /* divide by # cpus for max suspends */
 
-#define MAX_S2IDLE_CPU_ATTEMPTS  32   /* divide by # cpus for max suspends */
-
 static struct system_pm_ops *sys_pm_ops;
 
 
