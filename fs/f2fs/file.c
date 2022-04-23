8dcde22bb7be3980b498aab3c26aa802177d3809
Forest <forestd.github@gmail.com>
2022-04-23 03:00:40 +0000

    Merge branch 'linux-4.14.y' of https://github.com/jaegeuk/f2fs-stable into eleven-test
    Signed-off-by: Disconnnected0 <womhateui@gmail.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -384,8 +384,7 @@ sync_nodes:
 	clear_inode_flag(inode, FI_APPEND_WRITE);
 flush_out:
 	if ((!atomic && F2FS_OPTION(sbi).fsync_mode != FSYNC_MODE_NOBARRIER) ||
-			(atomic && !test_opt(sbi, NOBARRIER) &&
-					f2fs_sb_has_blkzoned(sbi)))
+	    (atomic && !test_opt(sbi, NOBARRIER) && f2fs_sb_has_blkzoned(sbi)))
 		ret = f2fs_issue_flush(sbi, inode->i_ino);
 	if (!ret) {
 		f2fs_remove_ino_entry(sbi, ino, UPDATE_INO);
