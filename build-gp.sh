2d5995c4feca0b27835d42006031b869953951bc
Forest <forestd.github@gmail.com>
2022-07-25 07:09:02 +0000

    build: revise the 2 other script too

--- build-gp.sh
+++ build-gp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#
-# Compile script for ConsIstenX/QuicksilveR kernel
-# Copyright (C) 2020-2021 Adithya R.
-# Made this for Gitpod Users
-# Because of Reinstallation the TC again after the Workspace is Closed
-# This Modifed TC_DIR Would save you some Precious Time - Disconnect0
 
+# Script for compiling Cons!stenX Kernel
+# Copyright (c) 2022 Disconnect0 <forestd.github@gmail.com>
+# Based on ghostrider-reborn script
+# Copyright (C) 2020-2021 Adithya R.
+
+# Variables
 SECONDS=0 # builtin bash timer
 ZIPNAME="CIsX~v1-Ginkgo|Willow-$(TZ=Asia/Manila date +"%Y%m%d-%H%M").zip"
 TC_DIR="/workspace/tc"
@@ -26,6 +26,7 @@ echo -e "\nCompiling with $($TC_DIR/bin/clang --version | head -n1 | cut -d " " 
 
 export KBUILD_BUILD_USER=forest
 export KBUILD_BUILD_HOST=Disconnect0
+export KBUILD_BUILD_VERSION="1"
 
 if [[ $1 = "-r" || $1 = "--regen" ]]; then
 make O=out ARCH=arm64 $DEFCONFIG savedefconfig
@@ -67,32 +68,35 @@ sed -i 's/tristate/bool/;s/default m/default y/;' net/wireguard/Kconfig
 touch net/wireguard/.check
 fi
 
+# Compilation
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
 
+# Checking Files
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nCompiled Successfully With $($TC_DIR/bin/clang --version | head -n1 | cut -d " " -f1,4)! Zipping up...\n"
+
+# Zipping
 if [ -d "$AK3_DIR" ]; then
 cp -r $AK3_DIR AnyKernel3
-elif ! git clone -q https://github.com/Disconnect0/AnyKernel3; then
-echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
-exit 1
+echo -e "\nAnyKernel3 not found inside the repo! Aborting..."
 fi
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
 cp out/arch/arm64/boot/dtbo.img AnyKernel3
 rm -f *zip
 cd AnyKernel3
-git checkout master &> /dev/null
 zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
 rm -rf AnyKernel3
 rm -rf out/arch/arm64/boot
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 echo "Zip: $ZIPNAME"
-if ! [[ $HOSTNAME = "RyzenBeast" && $USER = "adithya" ]]; then
+if ! [[ $HOSTNAME = "KOMPUTERTWU" && $USER = "forest" ]]; then
+
+# Uploading
 curl -T $ZIPNAME temp.sh; echo
 echo " "
 curl --upload-file $ZIPNAME https://transfer.sh/$ZIPNAME; echo
--- build-sdclang.sh
+++ build-sdclang.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-#
-# Compile script for QuicksilveR kernel
+
+# Script for compiling Cons!stenX Kernel
+# Copyright (c) 2022 Disconnect0 <forestd.github@gmail.com>
+# Based on ghostrider-reborn script
 # Copyright (C) 2020-2021 Adithya R.
 
+# Variables
 SECONDS=0 # builtin bash timer
 ZIPNAME="CIsX~v1-Ginkgo|Willow-$(TZ=Asia/Manila date +"%Y%m%d-%H%M").zip"
 TC_DIR="$HOME/tc/sdclang"
@@ -13,7 +16,7 @@ DEFCONFIG="vendor/ginkgo-perf_defconfig"
 
 # Install libncurses5 and dev version of it
 sudo apt install -y libncurses5 \
-libncurses5-dev
+                    libncurses5-dev
 
 export PATH="${TC_DIR}/compiler/bin:${GCC64_DIR}/bin:${GCC_DIR}/bin:/usr/bin:${PATH}"
 
@@ -45,6 +48,7 @@ echo -e "\nCompiling with $($TC_DIR/bin/clang --version | head -n1 | cut -d " " 
 
 export KBUILD_BUILD_USER=forest
 export KBUILD_BUILD_HOST=Disconnect0
+export KBUILD_BUILD_VERSION="1"
 
 if [[ $1 = "-r" || $1 = "--regen" ]]; then
 make O=out ARCH=arm64 $DEFCONFIG savedefconfig
@@ -89,22 +93,23 @@ fi
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
+# Compilation
 echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) O=out ARCH=arm64 LD_LIBRARY_PATH="${TC_DIR}/lib:${LD_LIBRARY_PATH}" CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image.gz-dtb dtbo.img
 
+# Checking Files
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nCompiled Successfully With $($TC_DIR/bin/clang --version | head -n1 | cut -d " " -f1,4)! Zipping up...\n"
+
+# Zipping
 if [ -d "$AK3_DIR" ]; then
 cp -r $AK3_DIR AnyKernel3
-elif ! git clone -q https://github.com/Disconnect0/AnyKernel3; then
-echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
-exit 1
+echo -e "\nAnyKernel3 not found inside the repo! Aborting..."
 fi
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
 cp out/arch/arm64/boot/dtbo.img AnyKernel3
 rm -f *zip
 cd AnyKernel3
-git checkout master &> /dev/null
 zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
 rm -rf AnyKernel3
@@ -112,6 +117,8 @@ rm -rf out/arch/arm64/boot
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 echo "Zip: $ZIPNAME"
 if ! [[ $HOSTNAME = "KOMPUTERTWU" && $USER = "forest" ]]; then
+
+# Uploading
 curl -T $ZIPNAME temp.sh; echo
 echo " "
 curl --upload-file $ZIPNAME https://transfer.sh/$ZIPNAME; echo
